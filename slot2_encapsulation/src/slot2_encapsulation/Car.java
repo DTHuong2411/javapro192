00b9f12ce81de19e928bee4d8299c9c7f1328c48
Dang Thu Huong <huongdthe182134@fpt.edu.vn>
2023-09-07 14:57:53 +0700

    Slot2

---
+++ slot2_encapsulation/src/slot2_encapsulation/Car.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package slot2_encapsulation;
+
+public class Car
+{
+      
+        // private variable  
+        private String name;
+      
+        // getter method for name  
+        public String getName()
+    {
+           return name;
+          
+    }
+      
+        // setter method for name  
+        public void setName(String name)
+    {
+           this.name = name;  
+    }
+      
+}
---
+++ slot2_encapsulation/src/slot2_encapsulation/Slot2_encapsulation.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package slot2_encapsulation;
+
+/**
+ *
+ * @author nunhu
+ */
+public class Slot2_encapsulation 
+{
+       public static void main(String[] args)
+    {
+          
+            // creating instance of the encapsulated class  
+            Car car
+            = new Car();
+          
+            // setting value in the name member  
+            car.setName("Porche");
+          
+            // getting value of the name member  
+            System.out.println(car.getName());
+          
+    }
+      
+}
